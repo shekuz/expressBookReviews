51d5048a3ad968201735147b7b8cfd337f600fdd
shekuz <shekuprog@gmail.com>
2026-03-12 00:06:18 -0400

    Implemented book search by ISBN and author

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,63 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
-}
+  "9780385474542": {
+    "author": "Chinua Achebe",
+    "title": "Things Fall Apart",
+    "reviews": {}
+  },
 
-module.exports=books;
+  "9780140449913": {
+    "author": "Hans Christian Andersen",
+    "title": "Fairy tales",
+    "reviews": {}
+  },
+
+  "9780142437223": {
+    "author": "Dante Alighieri",
+    "title": "The Divine Comedy",
+    "reviews": {}
+  },
+
+  "9780140449197": {
+    "author": "Unknown",
+    "title": "The Epic Of Gilgamesh",
+    "reviews": {}
+  },
+
+  "9780199537829": {
+    "author": "Unknown",
+    "title": "The Book Of Job",
+    "reviews": {}
+  },
+
+  "9780140449388": {
+    "author": "Unknown",
+    "title": "One Thousand and One Nights",
+    "reviews": {}
+  },
+
+  "9780140447575": {
+    "author": "Unknown",
+    "title": "Njál's Saga",
+    "reviews": {}
+  },
+
+  "9780141439518": {
+    "author": "Jane Austen",
+    "title": "Pride and Prejudice",
+    "reviews": {}
+  },
+
+  "9780140440170": {
+    "author": "Honoré de Balzac",
+    "title": "Le Père Goriot",
+    "reviews": {}
+  },
+
+  "9780802144479": {
+    "author": "Samuel Beckett",
+    "title": "Molloy, Malone Dies, The Unnamable, the trilogy",
+    "reviews": {}
+  }
+};
+
+module.exports = books;
